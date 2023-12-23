38c5d905ea60fa33dfbd1103d1c4bf3cce46316c
wsigma21 <wallstlbl@gmail.com>
2023-12-23 19:55:25 +0900

    add react&typescript練習

--- practice2/src/App.tsx
+++ practice2/src/App.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import axios from "axios";
 import './App.css';
+import { Todo } from "./Todo"
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+
+const user = {
+  name: "sigma",
+  hobbies : ["囲碁", "将棋"]
+}
 
 function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) =>{
-      console.log(res.data);
+    axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos").then((res) =>{
+      setTodos(res.data);
     })
   }
   return (
     <div className="App">
+      <UserProfile user={user}/>
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed}></Todo>
+      ))}
     </div>
   );
 }
---
+++ practice2/src/Text.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+type TextType = {
+  color: string;
+  fontSize: string;
+}
+
+export const Text: FC<TextType> = (props) => {
+  const { color, fontSize } = props;
+  return (
+    <p style={{color, fontSize}}>テキストやで！</p>
+  )
+}
